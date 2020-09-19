555d44db28db3e658d327fe5e065c5717e902380
LMingL <2438124738@qq.com>
2020-09-19 09:53:48 +0800

    init CS 106a

---
+++ CS 106b stanford/CS106/build-CS106-Desktop_Qt_5_15_1_GCC_64bit-Debug/Library/private/build.h
@@ -0,0 +1,9 @@
+#ifndef SPL_BUILD_H
+#define SPL_BUILD_H
+
+#define SPL_VERSION "2020.1"
+#define SPL_INSTALL_LOCATION ""
+#define SPL_BUILD_DATE "Fri Sep 18 23:30:21 2020"
+#define SPL_BUILD_USER "lewin"
+
+#endif
